ae6cdfa35518a932197cad6f7237d7a0d4b7693c
Murtaza Salih <114719466+itsrody@users.noreply.github.com>
2025-05-23 11:36:49 +0300

    Create __init__.py

---
+++ core_modules/__init__.py
@@ -0,0 +1,17 @@
+# core_modules/__init__.py
+
+# This file makes the 'core_modules' directory a Python package.
+# It can be left empty or used to make sub-modules more easily accessible.
+
+# For example, you could expose key functions here if desired:
+# from .downloader import download_filter_lists
+# from .parser_validator import parse_and_validate_rules
+# from .rephraser import rephrase_rules
+# from .unifier_optimizer import unify_and_optimize_rules
+# from .generator import generate_brave_power_list
+# from .main_generator import main_workflow
+
+# However, for clarity in the main_generator.py, direct imports
+# from .module_name import function_name are often preferred.
+
+# No specific code is required in this __init__.py for the current setup.
