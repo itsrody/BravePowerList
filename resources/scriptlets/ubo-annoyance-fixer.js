4fda98ac1bcacaf3f54a4986532d7af1b6511a53
Murtaza Salih <114719466+itsrody@users.noreply.github.com>
2025-05-23 11:44:03 +0300

    Create ubo-annoyance-fixer.js

---
+++ resources/scriptlets/ubo-annoyance-fixer.js
@@ -0,0 +1,11 @@
+// resources/scriptlets/ubo-annoyance-fixer.js
+// Name: ubo-annoyance-fixer.js
+// Kind: application/javascript
+// Purpose: Placeholder for a generic annoyance fixing scriptlet.
+
+(function() {
+    'use strict';
+    // console.log('[Brave Adblock Scriptlet: ubo-annoyance-fixer.js] Executed.');
+    // Actual implementation would be complex and target specific website annoyances.
+    // This is a placeholder.
+})();
