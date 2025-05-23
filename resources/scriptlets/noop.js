cfe8b7d87826d283a9f1cf5917f53b67af6cd14d
Murtaza Salih <114719466+itsrody@users.noreply.github.com>
2025-05-23 11:42:16 +0300

    Create noop.js

---
+++ resources/scriptlets/noop.js
@@ -0,0 +1,12 @@
+// resources/scriptlets/noop.js
+// Name: noop.js
+// Aliases: blank.js, empty.js
+// Kind: application/javascript
+// Purpose: No operation. Used to replace unwanted scripts.
+
+(function() {
+    'use strict';
+    // Intentionally does nothing.
+    // This scriptlet is used to neutralize other scripts by redirecting them here.
+    // console.log('[Brave Adblock Scriptlet: noop.js] Executed.'); // Optional: for debugging if it's being hit
+})();
